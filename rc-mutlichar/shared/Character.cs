19876bd6ce0a4d75654236f8c9efaed0ceb30ce4
Luke <62490330+Luke20201@users.noreply.github.com>
2023-07-15 19:42:48 +0100

    Add files via upload

---
+++ rc-mutlichar/shared/Character.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalClasses
+{
+    public class Character
+    {
+        public string License { get; set; } = null;
+        public string Slot { get; set; } = null;
+        public string CitizenID { get; set; } = null;
+        public string FirstName { get; set; } = null;
+        public string LastName { get; set; } = null;
+        public string DateOfBirth { get; set; } = null;
+        public string Gender { get; set; } = null;
+        public string Job { get; set; } = null;
+        public string BankBalance { get; set; } = null;
+        public string CashBalance { get; set; } = null;
+        public string LastLocation { get; set; } = null;
+    }
+}
---
+++ rc-mutlichar/shared/NUIEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalClasses
+{
+    public class NUIEvent
+    {
+        public string Name { get; set; }
+        public object Data { get; set; }
+    }
+}
